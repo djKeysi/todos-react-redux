872bc435f01c05cdeb9d4f1560edef88d07fb9b7
Зенченко Евгений <zenchenko_es@outlook.com>
2025-05-04 02:34:14 +0700

    end redux todos2

--- src/components/reducers/todos-loading-error-reducer.jsx
+++ src/components/reducers/todos-loading-error-reducer.jsx
@@ -1,28 +1,3 @@
-// export const initialLoadingErrorTodosState = {
-// 	loading: true,
-// 	error: '',
-// };
-
-// export const todosLoadingErrorReducer = (
-// 	state = initialLoadingErrorTodosState,
-// 	action,
-// ) => {
-// 	switch (action.type) {
-// 		case 'LOADING_TODOS':
-// 			return {
-// 				//...state,
-// 				loading: action.payload,
-// 			};
-// 		case 'ERROR_TODOS':
-// 			return {
-// 				//...state,
-// 				error: action.payload,
-// 			};
-// 		default:
-// 			state;
-// 	}
-// };
-
 export const initialLoadingErrorTodosState = {
 	loading: true,
 	error: '',
@@ -35,7 +10,7 @@ export const todosLoadingErrorReducer = (
 	switch (action.type) {
 		case 'LOADING_TODOS':
 			return {
-				...state,
+				//...state,
 				loading: action.payload,
 			};
 		case 'ERROR_TODOS':
